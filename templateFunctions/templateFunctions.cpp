f862b1577b35bacb0d892f5046ad1bbbaf53724c
Justin This <justinthis8@gmail.com>
2023-11-28 20:23:34 -0500

    Created overloaded int version of half function. Updated driver.

--- templateFunctions/templateFunctions.cpp
+++ templateFunctions/templateFunctions.cpp
@@ -2,6 +2,10 @@
 #include <cmath>
 using namespace std;
 
+// Function prototype.
+int half(int);
+
+// Template function for non-integral data types.
 template <typename T>
 T half(T num) {
     return num / 2;
@@ -9,6 +13,12 @@ T half(T num) {
 
 int main() {
     cout << half(25.0) << endl;
+    cout << half(14.17f) << endl;;
+    cout << half(25) << endl;;
 
     return 0;
+}
+
+int half(int num) {
+    return round(static_cast<double>(num) / 2);
 }
